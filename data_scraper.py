df451bbbed4ceaeb2979158db6ad256aee71b0c7
RobCac <52357124+RobCac@users.noreply.github.com>
2020-12-22 15:16:10 +0200

    First version of etuovi data miner

--- data_scraper.py
+++ data_scraper.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+
 
 def check_element(id): #takes an element-id and returns true if it can be found
     try:
@@ -12,39 +14,56 @@ def check_element(id): #takes an element-id and returns true if it can be found
         return False
     return True
 
+def etuovi_get_apartments():
+    #Reads username and pw for login
+    f = open("user.txt","r")
+    lines = f.readlines()
+    user_name = lines[0]
+    password = lines[1]
+    f.close()
+    #create dataframe. In future will come as input?
+    apart_cols = ['Osoite', 'Vmh', 'Pinta-ala', 'URL']
+    df = pd.DataFrame(columns = apart_cols)
 
-#Reads username and pw for login
-f = open("user.txt","r")
-lines = f.readlines()
-user_name = lines[0]
-password = lines[1]
-f.close()
+    #logs in to etuovi.com account
+    driver = webdriver.Firefox()
+    driver.get("https://www.etuovi.com")
 
+    #wait for cookie popup
+    driver.implicitly_wait(10) 
+    driver.find_element_by_id("almacmp-modalConfirmBtn").click()
 
-#logs in to etuovi.com account
-driver = webdriver.Firefox()
-driver.get("https://www.etuovi.com")
+    #Log in with read credentials
+    element = driver.find_element_by_link_text("Kirjaudu")
+    element.click()
+    element = driver.find_element_by_id("alma-tunnus-username")
+    element.send_keys(user_name)
+    element = driver.find_element_by_id("alma-tunnus-password")
+    element.send_keys(password)
+    element.send_keys(Keys.RETURN)
 
-#wait for cookie popup
-driver.implicitly_wait(10) 
-driver.find_element_by_id("almacmp-modalConfirmBtn").click()
+    #Confirm log in
+    element = driver.find_element_by_id("alma-tunnus-notification-button-close")
+    element.click()
 
-#Log in with read credentials
-element = driver.find_element_by_link_text("Kirjaudu")
-element.click()
-element = driver.find_element_by_id("alma-tunnus-username")
-element.send_keys(user_name)
-element = driver.find_element_by_id("alma-tunnus-password")
-element.send_keys(password)
-element.send_keys(Keys.RETURN)
+    #Go to saved search
+    driver.get("https://www.etuovi.com/myytavat-asunnot?haku=P1548857208")
 
-#Confirm log in
-element = driver.find_element_by_id("alma-tunnus-notification-button-close")
-element.click()
+    #get all listings
+    apartments = driver.find_elements_by_class_name("styles__cardLink__2Oh5I")
 
-#Go to saved search
-driver.get("https://www.etuovi.com/myytavat-asunnot?haku=P1548857208")
+    links = []
+    for result in apartments:
+        links.append(result.get_attribute('href'))
 
-apartments = driver.find_elements_by_class_name("ListPage__cardContainer__39dKQ")
-print(len(apartments))
-element.close()
+    for link in links:
+        driver.get(link)
+        osoite_ele = driver.find_element_by_xpath('/html/body/div[2]/div/div[3]/div/section/div[2]/div/div/div[3]/div[2]/div[1]/div/div[1]/div/div[1]/h1')
+        osoite = osoite_ele.text
+        vmh_ele = driver.find_element_by_xpath('//*[@id="previousDebtFreePrice"]')
+        vmh = vmh_ele.text
+        pinta_ele = driver.find_element_by_xpath('/html/body/div[2]/div/div[3]/div/section/div[2]/div/div/div[3]/div[2]/div[3]/div[2]/div[2]/div/div/div/div/div[1]/div[7]/div[2]/span')
+        pinta = pinta_ele.text
+        kohde = {'Osoite' : osoite, 'Vmh' : vmh, 'Pinta-ala' : pinta , 'URL' : link}
+        df = df.append(kohde, ignore_index=True)
+    print(df)
