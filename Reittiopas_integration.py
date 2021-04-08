8dbc795d1fb70bd62b0e715ef6d3ce5eb2fc0317
RobCac <52357124+RobCac@users.noreply.github.com>
2021-04-08 17:04:36 +0300

    Added reittiopas integration
    Added Reittiopas_integration.py that uses the API provided by hsl to check the journey lengths to a few key locations from the apartment and reports them back

---
+++ Reittiopas_integration.py
@@ -0,0 +1,77 @@
+import json
+import pandas as pd
+import string
+import requests
+
+
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+def create_query_for_graphQL(api_adress, destination, time = '"07:00:00"'):
+    #creates the GraphQLrequest for traveltime between to locations in ReittiopasAPI format
+    query = """
+    {{
+    plan(
+        fromPlace: "{starter}",
+        toPlace: "{to}",
+        date: "2021-4-8",
+        time: {depart},
+        numItineraries: 3,
+    ) {{
+        itineraries{{
+        duration
+        }}
+    }}
+    }}
+    """.format(starter = str(api_adress), to = str(destination), depart = str(time))
+    return query
+
+
+
+def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
+    request = requests.post('https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql', json={'query': query})
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+
+
+
+
+def route_length(apart_adress, home = "Kurtinmäenkuja 2, Espoo::60.178746,24.595081", work = "Valimotie 21, Helsinki::60.220108,24.87883" ):
+
+    url = "https://api.digitransit.fi/geocoding/v1/search?text="
+    url = url + apart_adress + "&layers=address&size=1"
+    response = requests.get(url)
+    coordinates = response.json()["bbox"]
+    api_adress= apart_adress+"::"+str(coordinates[1]) + "," + str(coordinates[0])
+    
+    
+    home_query = create_query_for_graphQL(api_adress,home)
+    home_traveljson = run_query(home_query)
+    home_durations = home_traveljson["data"]["plan"]["itineraries"]
+    home_average_travel = 0
+    for dictionary in home_durations:
+        home_average_travel = home_average_travel + dictionary["duration"]
+    home_average_travel = int(home_average_travel/(3*60))
+
+    
+    work_query = create_query_for_graphQL(api_adress,work)
+    work_traveljson = run_query(work_query)
+    work_durations = work_traveljson["data"]["plan"]["itineraries"]
+    work_average_travel = 0
+    for dictionary in work_durations:
+        work_average_travel = work_average_travel + dictionary["duration"]
+    work_average_travel = int(work_average_travel/(3*60))
+    return home_average_travel, work_average_travel
+
+
+
+
+        
+
+
+route_length("valimotie 21, Helsinki")
---
+++ dataframe_cleaner.py
@@ -0,0 +1,15 @@
+import pandas as import pd
+import numpy as np
+import data_scraper as ds
+
+
+def clean_dataframetoSQL(df):
+    for 
+
+
+
+
+
+
+df = pd.read_csv('data.csv')
+clean_dataframetoSQL(df)
